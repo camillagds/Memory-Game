e3eb0939b8655d84d5bfe587753d7b607999062b
camillagds <milla@silverwolf.com.br>
2017-01-04 04:07:28 -0200

    update travis

--- test_app.sh
+++ test_app.sh
@@ -1,5 +1,5 @@
  sleep 5
- if curl web | grep -q 'Click on a tile.'; then
+ if curl "http://localhost:8080" | grep -q 'Click on a tile.'; then
    echo "Tests passed!"
    curl -X GET "http://localhost:8080"
    exit 0
